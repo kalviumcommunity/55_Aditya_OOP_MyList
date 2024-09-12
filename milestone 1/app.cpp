47baca936b5d5ed229874a3154e229f4719d8d05
kalvian2 <aditya.kalvium@community>
2024-09-12 10:54:59 +0530

    Encapsulation

--- milestone 1/app.cpp
+++ milestone 1/app.cpp
@@ -9,37 +9,50 @@ private:
     string title;
     string description;
     bool isCompleted;
-    
+
 public:
     static int taskCount;
 
     // Constructor
     Task(string t, string d)
         : title(t), description(d), isCompleted(false) {
-        taskCount++;  
-    }
-
-    Task& setTitle(const string& t) {
-        this->title = t;
-        return *this;
-    }
-
-    Task& setDescription(const string& d) {
-        this->description = d;
-        return *this;
-    }
-
-    void markAsCompleted() {
-        this->isCompleted = true;
-    }
-
-    void displayTask() const {
-        cout << "Title: " << title << ", Description: " << description
-             << ", Status: " << (isCompleted ? "Completed" : "Pending") << endl;
+        taskCount++;
     }
 
     string getTitle() const {
         return this->title;
+    }
+
+    void setTitle(const string& t) {
+        this->title = t;
+    }
+
+    string getDescription() const {
+        return this->description;
+    }
+
+  
+    void setDescription(const string& d) {
+        this->description = d;
+    }
+
+    
+    bool getIsCompleted() const {
+        return this->isCompleted;
+    }
+
+    
+    void setIsCompleted(bool completed) {
+        this->isCompleted = completed;
+    }
+
+    void markAsCompleted() {
+        setIsCompleted(true);  
+    }
+
+    void displayTask() const {
+        cout << "Title: " << getTitle() << ", Description: " << getDescription()
+             << ", Status: " << (getIsCompleted() ? "Completed" : "Pending") << endl;
     }
 
     
@@ -53,9 +66,13 @@ int Task::taskCount = 0;
 class TaskManager {
 private:
     vector<Task> tasks;
-    
+
 public:
     static int completedTasksCount;
+
+    const vector<Task>& getTasks() const {
+        return this->tasks;
+    }
 
     void addTask(const Task& task) {
         this->tasks.push_back(task);
@@ -65,14 +82,14 @@ public:
         for (auto& task : this->tasks) {
             if (task.getTitle() == title) {
                 task.markAsCompleted();
-                completedTasksCount++;  
+                completedTasksCount++;
                 break;
             }
         }
     }
 
     void displayAllTasks() const {
-        for (const auto& task : tasks) {
+        for (const auto& task : getTasks()) {  
             task.displayTask();
         }
     }
@@ -102,7 +119,6 @@ int main() {
     cout << "\nAll tasks after marking some tasks as completed:" << endl;
     manager.displayAllTasks();
 
-    
     cout << "\nTotal number of tasks created: " << Task::getTaskCount() << endl;
     cout << "Total number of tasks completed: " << TaskManager::completedTasksCount << endl;
 
